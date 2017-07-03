7461be29bf53ed644ea9f57252508d72fe8f8a7d
Luke Mico <lukemico@me.com>
2017-07-03 21:11:10 +1000

    back to top button

--- js/main.js
+++ js/main.js
@@ -40,6 +40,25 @@ $('a[href*="#"]')
     }
   });
 
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("myBtn").style.display = "block";
+  } else {
+    document.getElementById("myBtn").style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Chrome, Safari and Opera
+  document.documentElement.scrollTop = 0; // For IE and Firefox
+}
+
 // <div id="page-wrap">
 //
 //   <h1 id="top"></h1>
